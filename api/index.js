bf8e5cf4a70723451704849db39a07d5bf4ecaa1
Alam Rodriguez <alamrd2016@gmail.com>
2024-03-16 01:51:25 -0400

    header to allow headers added

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const app = express();
 // app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // Puedes ajustar '*' según tus necesidades
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
